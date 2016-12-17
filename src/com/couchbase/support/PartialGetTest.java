0ddea85de7a60a023db96bd945e89502b8a3098f
couchbasebrian <brian.williams@couchbase.com>
2016-12-16 16:18:58 -0800

    new approach

--- src/com/couchbase/support/PartialGetTest.java
+++ src/com/couchbase/support/PartialGetTest.java
@@ -2,8 +2,8 @@ package com.couchbase.support;
 
 // Brian Williams
 // October 5, 2016
+// Updated December 16, 2016
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -11,14 +11,15 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.print.Doc;
+import rx.functions.Func1;
 
-import rx.Observable;
-
+import com.couchbase.client.core.BackpressureException;
+import com.couchbase.client.core.time.Delay;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.LegacyDocument;
+import com.couchbase.client.java.util.retry.RetryBuilder;
 
 // If using maven, use this dependency to get the 2.3.4 SDK 
 
@@ -39,7 +40,7 @@ public class PartialGetTest {
 
 		PartialGetTest tester = new PartialGetTest();
 
-		Cluster cluster = CouchbaseCluster.create("hostname");
+		Cluster cluster = CouchbaseCluster.create("172.23.99.170");
 		Bucket bucket = cluster.openBucket("BUCKETNAME");
 
 		// Do a number of tests in 100ms increments
@@ -53,7 +54,7 @@ public class PartialGetTest {
 		tester.doExample(bucket, 800);
 		tester.doExample(bucket, 900);
 		tester.doExample(bucket, 1000);
-		
+
 		tester.doExample(bucket, 2000);
 		tester.doExample(bucket, 3000);
 
@@ -85,24 +86,47 @@ public class PartialGetTest {
 		AtomicInteger counter = new AtomicInteger();
 
 		long startTime = System.currentTimeMillis();
-		
-		Observable.from(theKeys).flatMap(id -> { counter.incrementAndGet(); return bucket.async().get(LegacyDocument.create(id)); }) 
-		.doOnNext(doc -> results.put(doc.id(), doc.content())) 
-		.last() 
-		.timeout(timeoutInMilliseconds, TimeUnit.MILLISECONDS)
-		.toBlocking() 
-		.subscribe(doc -> { 
-			// this is a javax.print.Doc
-			System.out.println("doc: " + Doc.class.getName());
-		}, throwable -> { 
-			if (throwable instanceof TimeoutException) { 		
-				System.out.println("Got an expected timeout exception after " + ( System.currentTimeMillis() - startTime) + "ms.");
-				isTimeout.set(true); 
+
+		// new way from http://hastebin.com/nugufeculi.java
+		rx.Observable.from(theKeys).flatMap(id ->
+		{
+			counter.incrementAndGet();
+			return rx.Observable.defer(() -> bucket.async().get(LegacyDocument.create(id))
+					.retryWhen(RetryBuilder.anyOf(BackpressureException.class) //only retry on Backpressure
+							.max(2) //you could tune the maximum number of attempts here
+							.delay(Delay.exponential(TimeUnit.MILLISECONDS, 1, 2)) //delay between attempts grows exponentially
+							.build())
+							.onErrorResumeNext(new Func1<Throwable, rx.Observable<? extends LegacyDocument>>() {
+								@Override
+								public rx.Observable<? extends LegacyDocument> call(Throwable throwable) {
+									if (throwable instanceof TimeoutException) {
+										isTimeout.set(true);
+										//if one request is slow and times out, we should not proactively cancel other requests
+										return rx.Observable.just(null);
+									} else {
+										return rx.Observable.error(throwable);
+									}
+								}
+							}));
+		})
+		.doOnNext(doc -> {
+			if (doc != null) {
+				results.put(doc.id(), doc.content());
 			}
-			else {
+		})
+		.timeout(500, TimeUnit.MILLISECONDS)
+		.last()
+		.toBlocking()
+		.subscribe(doc -> {
+		}, throwable -> {
+			if (throwable instanceof BackpressureException) {
+				System.out.println("Got back pressure exception after " + (System.currentTimeMillis() - startTime) + "ms.");
+			} else {
 				System.out.println("Unexpected throwable: " + throwable.getClass().getName());
 			}
-		});		
+		});
+
+		System.out.println(isTimeout.get());
 
 		System.out.println("After " + (System.currentTimeMillis() - startTime) + "ms, the size of results is " + results.size() + " and counter is " + counter.get());
 
@@ -135,7 +159,7 @@ public class PartialGetTest {
 		}
 
 		System.out.println();
-		
+
 	} // doExample()
 
 }
